5cc5aed3983b1e89d0f5e86304801d12ce507eb5
ElLuchoMan <baluist@correo.udistrital.edu.co>
2022-12-11 21:56:48 -0500

    Se alista formulario reactivo para registrar pacientes

---
+++ src/app/models/paciente.model.ts
@@ -0,0 +1,13 @@
+export interface  Paciente {
+  nombre: string;
+  apellido: string;
+  tipoDocumento: string;
+  documento: number;
+  telefono: number;
+  celular: number;
+  fechaNacimiento: Date;
+  sexo: string;
+  email: string;
+  estado: string;
+  multa: number;
+}
--- src/app/pages/pacientes/registrar/registrar.component.ts
+++ src/app/pages/pacientes/registrar/registrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+import {Paciente} from "../../../models/paciente.model";
 
 @Component({
   selector: 'app-registrar',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar.component.scss']
 })
 export class RegistrarComponent implements OnInit {
+  tiposDocumento:any= [
+    {id: 'CC', nombre: 'Cédula de ciudadanía'},
+    {id: 'CE', nombre: 'Cédula de extranjería'},
+    {id: 'P', nombre: 'Pasaporte'},
+    {id: 'PE', nombre: 'Permiso especial de permanencia'},
+    {id: 'TI', nombre: 'Tarjeta de identidad'}
+  ];
+  constructor(private fb: FormBuilder, private router: Router) { }
+  registroUsuarios: FormGroup= this.fb.group({
+    nombre: [''],
+    apellido: [''],
+    tipoDocumento: [''],
+    documento: [''],
+    telefono: [''],
+    celular: [''],
+    fechaNacimiento: [''],
+    sexo: [''],
+    email: [''],
 
-  constructor() { }
-
+  });
   ngOnInit(): void {
   }
 
+  registrarPaciente(){
+    const paciente: Paciente ={
+      nombre: this.registroUsuarios.get('nombre')?.value,
+      apellido: this.registroUsuarios.get('apellido')?.value,
+      tipoDocumento: this.registroUsuarios.get('tipoDocumento')?.value,
+      documento: this.registroUsuarios.get('documento')?.value,
+      telefono: this.registroUsuarios.get('telefono')?.value,
+      celular: this.registroUsuarios.get('celular')?.value,
+      fechaNacimiento: this.registroUsuarios.get('fechaNacimiento')?.value,
+      sexo: this.registroUsuarios.get('sexo')?.value,
+      email: this.registroUsuarios.get('email')?.value,
+      estado: 'Activo',
+      multa:0,
+    }
+    console.table(paciente);
+  }
 }
